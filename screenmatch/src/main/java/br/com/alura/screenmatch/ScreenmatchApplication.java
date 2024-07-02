a6c76e6ccc630e7073102ae099b910708fc572df
Graziele Garcia <grazycgarcia@gmail.com>
2024-07-02 20:02:34 -0300

    Starting project

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DataSeries;
+import br.com.alura.screenmatch.service.ApiConsumption;
+import br.com.alura.screenmatch.service.DataConvert;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var apiConsumption = new ApiConsumption();
+		var json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=b19431bd");
+		System.out.println(json);
+		DataConvert converter = new DataConvert();
+		DataSeries data = converter.getData(json, DataSeries.class);
+		System.out.println(data);
+	}
+}
